f8838ffc245d665893835d1872728328115960e8
cocomongg <pjm4142@naver.com>
2021-02-07 01:58:34 +0900

    add practice2_(6)

--- practice2/src/main/java/hello/core/order/OrderServiceImpl.java
+++ practice2/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,14 +5,17 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
