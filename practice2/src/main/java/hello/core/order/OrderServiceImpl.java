a3591e575a97dfe0ec7fdac0550af9ba038ff8a1
cocomongg <pjm4142@naver.com>
2021-02-21 20:13:12 +0900

    add practice2_(7)

--- practice2/src/main/java/hello/core/order/OrderServiceImpl.java
+++ practice2/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,21 +5,38 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService{
+@RequiredArgsConstructor //롬복적용
+public class OrderServiceImpl implements OrderService {
 
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    /*
+    * 생성자 주입
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
+
+    /*
+    * 수정자 주입(의존관계)
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -31,7 +48,7 @@ public class OrderServiceImpl implements OrderService{
     }
 
     //싱글톤 - 테스트 용도
-    public MemberRepository getMemberRepository(){
+    public MemberRepository getMemberRepository() {
         return memberRepository;
     }
 }
