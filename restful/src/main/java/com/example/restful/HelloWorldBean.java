18daccc3dd23e94fdac44cf0712f13f2701cca80
cocomongg <pjm4142@naver.com>
2021-03-03 03:16:19 +0900

    setting project and test

---
+++ restful/src/main/java/com/example/restful/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.example.restful;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+
+}
+/**
+* @Data -> @Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualAndHashCode 를 한꺼번에 설정해줌
+* <생성자>
+* @NoArgsConstructor -> 파라미터가 없는 기본 생성자를 생성해줌
+* @AllArgsConstructor -> 모든 필드 값을 파라미터로 받는 생성자를 만들어줌
+* @RequiredArgsConstructor -> final이나 @NonNull인 필드 값만 파라미터로 받는 생성자를 만들어줌
+* */
---
+++ restful/src/main/java/com/example/restful/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.example.restful;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //GET
+    // /hello-world (endpoint)
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello Wolrd";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    } //@Controller와 달리 @RestController를 사용하게 되면 @Responsebody에 포함하지 않아도 자동으로 json 포맷으로 변함
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){ //{name}과 다른 파라미터를 설정할 경우 @PathVariable(value="name") String another_name
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+}
+
